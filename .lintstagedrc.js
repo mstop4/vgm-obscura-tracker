32fd734364620643fca7d7f21b240dbdd34b348d
M.S.T.O.P <mstop4@gmail.com>
2023-07-26 01:12:34 -0400

    clean up template

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,14 +1,14 @@
 module.exports = {
   // Type check TypeScript files
   '**/*.(ts|tsx)': () => [
+    `npm run prettier`,
     'npm run check-ts',
     `npm run lint`,
-    `npm run prettier`,
     // `npm run test`
   ],
 
-  // '**/*.scss': () => [
-  //   `npm run lint`,
-  //   `npm run prettier`,
-  // ],
+  '**/*.scss': () => [
+    `npm run lint`,
+    `npm run prettier`,
+  ],
 };
